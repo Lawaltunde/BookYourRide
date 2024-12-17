8972fcd0267619cdfbef4b8a393cdfcb8c63a9be
Lawal Tunde <hlawal@mycambrian.ca>
2024-12-17 15:10:58 -0500

    User model

---
+++ bookYourRide/src/com/sudburyRide/user/User.java
@@ -0,0 +1,51 @@
+package com.sudburyRide.user;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private String userName;
+
+    public User(UUID id, String userName) {
+        this.id = id;
+        this.userName = userName;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(userName, user.userName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userName);
+    }
+}
---
+++ bookYourRide/src/com/sudburyRide/user/UserService.java
@@ -0,0 +1,24 @@
+package com.sudburyRide.user;
+
+import java.util.UUID;
+
+public class UserService {
+    private final UserDAO userDAO = new UserDAO();
+
+
+    public User[] getAllUsers(){
+        if (userDAO.getUsers().length == 0){
+            return new User[0];
+        }
+        return userDAO.getUsers();
+    }
+
+    public User getUser(UUID id){
+        for (User user : getAllUsers()) {
+            if(user.getId().equals(id)){
+                return user;
+            }
+        }
+        throw new IllegalArgumentException("User id can't be null!");
+    }
+}
